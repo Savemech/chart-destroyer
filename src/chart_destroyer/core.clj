136117058cea401922df0438a0e87602076a1c99
root <root@MSHP.localdomain>
2023-10-17 13:59:51 +0300

    init

---
+++ src/chart_destroyer/core.clj
@@ -0,0 +1,6 @@
+(ns chart-destroyer.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/chart_destroyer/core_test.clj
@@ -0,0 +1,7 @@
+(ns chart-destroyer.core-test
+  (:require [clojure.test :refer :all]
+            [chart-destroyer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
